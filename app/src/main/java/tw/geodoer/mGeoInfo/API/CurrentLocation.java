c7b3e951aab3408e0523ca0e5137cf48b97b5ec3
FuYaoDe <dan82625@gmail.com>
2015-04-15 01:40:53 +0800

    新增取距離5秒餘時

--- app/src/main/java/tw/geodoer/mGeoInfo/API/CurrentLocation.java
+++ app/src/main/java/tw/geodoer/mGeoInfo/API/CurrentLocation.java
@@ -2,8 +2,7 @@ package tw.geodoer.mGeoInfo.API;
 
 import android.content.Context;
 import android.location.Location;
-
-import com.google.android.gms.maps.model.LatLng;
+import android.os.Handler;
 
 /**
  * Created by fud on 2015/4/14.
@@ -14,12 +13,23 @@ public class CurrentLocation implements GPSCallback {
     private Double lat = -1d;
     private Double lng = -1d;
     private Context context;
+    private Handler mHandle;
+    private Thread t;
+    private Boolean isThreadRun=true;
 
     public CurrentLocation(Context context){
         this.context=context;
         gpsManager = new GPSManager();
         gpsManager.startListening(context);
         gpsManager.setGPSCallback(this);
+
+        mHandle = new Handler();
+        t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                if(isThreadRun)mDis.onGetDistance((double)-1);
+            }
+        });
     }
 
     private onDistanceListener mDis=null;
@@ -36,16 +46,23 @@ public class CurrentLocation implements GPSCallback {
         this.lng=lng;
         gpsManager.startListening(context);
         gpsManager.setGPSCallback(this);
+        isThreadRun=true;
+        setTimeOut(5000);
+    }
+
+    public void setTimeOut(int s){
+        mHandle.postDelayed(t,s);
     }
 
     public void stopGps(){
+        isThreadRun=false;
         gpsManager.stopListening();
+        gpsManager.setGPSCallback(null);
     }
 
     public void onGPSUpdate(Location location) {
-        mDis.onGetDistance(DistanceCalculator.haversine(location.getLatitude(),location.getLongitude(),lat,lng));
-        gpsManager.stopListening();
-        gpsManager.setGPSCallback(null);
+        stopGps();
+        mDis.onGetDistance(DistanceCalculator.haversine(location.getLatitude(), location.getLongitude(), lat, lng));
     }
 
 
