917b97744ce04ab4314a3472611fef03494b0556
hem1102 <hem1102@hotmail.com.tw>
2015-04-16 01:19:23 +0800

    prpr

--- app/src/main/java/tw/geodoer/mPriority/controller/PriorityUpdater.java
+++ app/src/main/java/tw/geodoer/mPriority/controller/PriorityUpdater.java
@@ -34,17 +34,16 @@ public class PriorityUpdater
     }
     public void PirorityUpdate()
     {
-//        get number of events
-//        int count = dbTaskHelper.getCount();
-//        if(count==0)return; //if no event can be change , stop
-//        //--------------------------------------------------------------------------------------
-//        HandlerThread mHandlerThread = new HandlerThread("PrU");
-//        mHandlerThread.start();
-//        Handler mHandler = new Handler(mHandlerThread.getLooper());
-//
-//        Runnable r =new update_thread_2(mContext);
-//        Thread mThread = new Thread(r);
-//        mHandler.post(mThread);
+        //get number of events
+        int count = dbTaskHelper.getCount();
+        if(count==0)return; //if no event can be change , stop
+        //--------------------------------------------------------------------------------------
+        HandlerThread mHandlerThread = new HandlerThread("PrU");
+        mHandlerThread.start();
+        Handler mHandler = new Handler(mHandlerThread.getLooper());
+        Runnable r =new update_thread_2(mContext);
+        Thread mThread = new Thread(r);
+        mHandler.post(mThread);
 
     }
 
@@ -59,7 +58,6 @@ public class PriorityUpdater
             this.mContext = con;
             this.dbLocationHelper = new DBLocationHelper(mContext);
             this.dbTaskHelper = new DBTasksHelper(mContext);
-
         }
 
         public void run()
@@ -79,13 +77,16 @@ public class PriorityUpdater
                         for (int task_id : ids)
                         {
                             int locID = dbTaskHelper.getItemInt(task_id, ColumnTask.KEY.location_id);
-                            if (locID == 0) {
+                            if (locID == 0)
+                            {
                                 PriorityCalculatorNew Cal = new PriorityCalculatorNew();
                                 long due_time, left_time;
+
                                 due_time = dbTaskHelper.getItemLong(task_id, ColumnTask.KEY.due_date_millis);
                                 if (due_time == 0) left_time = 0;
                                 else if (due_time - System.currentTimeMillis() <= 0) left_time = 0;
                                 else left_time = due_time - System.currentTimeMillis();
+
                                 dbTaskHelper.setItem(task_id, ColumnTask.KEY.priority, Cal.getweight(left_time, 0));
                             }
                             else
@@ -96,8 +97,8 @@ public class PriorityUpdater
                                 double distance = DistanceCalculator.haversine(lat, lon, itemlat, itemlon);
 
                                 PriorityCalculatorNew Cal = new PriorityCalculatorNew();
-
                                 long due_time, left_time;
+
                                 due_time = dbTaskHelper.getItemLong(task_id, ColumnTask.KEY.due_date_millis);
                                 if (due_time == 0) left_time = 0;
                                 else if (due_time - System.currentTimeMillis() <= 0) left_time = 0;
